0c8147108c9b6f495a54cb796f7c646c3b73785d
elmar8287 <L-mar@inbox.ru>
2021-09-21 19:19:57 +0400

    Fix linters issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./App.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
 
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
   </React.StrictMode>,
-  document.getElementById("root")
-)
+  document.getElementById("root"),
+);
