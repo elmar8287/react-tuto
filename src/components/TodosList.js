a12f789b20890bd7b47c9cd7041f92da2fd5d071
elmar8287 <L-mar@inbox.ru>
2021-09-21 19:43:28 +0400

    Class to Fun for TodoList file

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -2,34 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const {
-      todos,
-      handleChangeProps,
-      deleteTodoProps,
-      setUpdate,
-    } = this.props;
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={handleChangeProps}
-            deleteTodoProps={deleteTodoProps}
-            setUpdate={setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = (props) => (
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps={props.deleteTodoProps}
+        setUpdate={props.setUpdate}
+      />
+    ))}
+  </ul>
+);
 
 TodosList.propTypes = {
   todos: PropTypes.instanceOf(Array).isRequired,
